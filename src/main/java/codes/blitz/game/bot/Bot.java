f5b4a9700aa34201388b51be10f38e751fa2247b
Marie-Soleil_Jarry <MSJAR@ulaval.ca>
2026-01-17 15:28:44 -0500

    checking 5 tiles around to find best spore

--- src/main/java/codes/blitz/game/bot/Bot.java
+++ src/main/java/codes/blitz/game/bot/Bot.java
@@ -25,9 +25,8 @@ public class Bot {
     List<Action> actions = new ArrayList<>();
     TeamInfo myTeam = gameMessage.world().teamInfos().get(gameMessage.yourTeamId());
     spawnerManager.setCurrentSpawners(myTeam.spawners());
-    if(!firstActionDone && myTeam.spores().getFirst().biomass() >= myTeam.nextSpawnerCost()){
-      actions.add(spawnerManager.createSpawner(myTeam.spores().getFirst()));
-      firstActionDone = true;
+    if(myTeam.spawners().size() < 2 && !myTeam.spores().isEmpty()){
+      actions.add(spawnerManager.createSpawner(findBestSpawnerLocation(gameMessage, myTeam)));
     }
     if(myTeam.nutrients() >= spawnerManager.INITIAL_SPORE_BIOMASS && !myTeam.spawners().isEmpty()){
       actions.add(spawnerManager.produceSpore(myTeam.nutrients()));
@@ -46,5 +45,31 @@ public class Bot {
     }
     // You can clearly do better than the random actions above. Have fun!!
     return actions;
+  }
+
+  private Spore findBestSpawnerLocation(TeamGameState gameMessage, TeamInfo myTeam){
+      Spore bestSpore = myTeam.spores().getFirst();
+      int maxAvailableNutrients = 0;
+      for (Spore s : myTeam.spores()) {
+        //count available nutrients near spore
+
+        int currentNutrients = gameMessage.world().map().nutrientGrid()[s.position().x()][s.position().y()];
+        for (int i = 1; i < 5; i++) {
+            if ((s.position().x() + i >= gameMessage.world().map().width()) ||
+                s.position().x() - i < 0 ||
+                s.position().y() + i >= gameMessage.world().map().height() ||
+                s.position().y() - i < 0)
+                continue;
+            currentNutrients += gameMessage.world().map().nutrientGrid()[s.position().x() + i][s.position().y()];
+            currentNutrients += gameMessage.world().map().nutrientGrid()[s.position().x() - i][s.position().y()];
+            currentNutrients += gameMessage.world().map().nutrientGrid()[s.position().x()][s.position().y() + i];
+            currentNutrients += gameMessage.world().map().nutrientGrid()[s.position().x()][s.position().y() - i];
+            if (currentNutrients > maxAvailableNutrients) {
+                maxAvailableNutrients = currentNutrients;
+                bestSpore = s;
+            }
+        }
+      }
+      return bestSpore;
   }
 }
